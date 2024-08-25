2759e57121d6ca490f2420d8b2017a8bc8ff3c70
Tony Lee <leegreen123@naver.com>
2024-08-25 21:12:48 +0900

    add other files

---
+++ init.lua
@@ -0,0 +1,8 @@
+vim.env.LANG = "en_US.UTF-8"
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.copilot_assume_mapped = true
+vim.loader.enable()
+
+require("config")
+require("config.lazy")

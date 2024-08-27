07342cf5ba2aaee2caf0c9f928df0944f7509579
Tony Lee <leegreen123@naver.com>
2024-08-27 09:40:03 +0900

    fix tab completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -13,6 +13,12 @@ return {
       require("luasnip.loaders.from_vscode").lazy_load()
       luasnip.config.setup({})
 
+      -- Define a function to check if the cursor is at the beginning of the line or not
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
       cmp.setup({
         snippet = {
           expand = function(args)
@@ -32,6 +38,25 @@ return {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
           }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            local copilot_suggestion = require("copilot.suggestion")
+            if copilot_suggestion.is_visible() then
+              copilot_suggestion.accept()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback() -- This will insert a normal tab (4 spaces) if none of the conditions are met
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = {
           { name = "nvim_lsp" },
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -26,10 +26,6 @@ return {
         suggestion = {
           enabled = true,
           auto_trigger = true,
-          keymap = {
-            accept = "<Tab>",
-            next = "<S-Tab>",
-          },
         },
         panel = { enabled = false },
         filetypes = {

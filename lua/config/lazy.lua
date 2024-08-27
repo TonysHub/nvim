4d254b3d32068ed8135e87e8eee99397de04b3e2
Tony Lee <leegreen123@naver.com>
2024-08-27 15:49:25 +0900

    remove signcolumns

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,13 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = function()
+        vim.opt.signcolumn = "no"
+      end,
+    },
     -- import/override with your plugins
     { "rebelot/kanagawa.nvim" },
     { import = "plugins" },

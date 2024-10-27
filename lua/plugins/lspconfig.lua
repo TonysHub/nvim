0912f3d7a680e52e4f66fbc6b8f7bce96da08099
Tony Lee <leegreen123@naver.com>
2024-10-27 16:20:46 +0900

    add ts deps

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,12 +10,55 @@ return {
     require("mason-lspconfig").setup()
 
     local servers = {
+      -- ts_ls = {
+      --   settings = {
+      --     formatting = true,
+      --     formatoptions = {
+      --       tabSize = 2,
+      --       useTabs = false,
+      --     },
+      --   },
+      -- },
+      --- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
+      --- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
+      tsserver = {
+        enabled = false,
+      },
       ts_ls = {
+        enabled = false,
+      },
+      vtsls = {
+        -- explicitly add default filetypes, so that we can extend
+        -- them in related extras
+        filetypes = {
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
         settings = {
-          formatting = true,
-          formatoptions = {
-            tabSize = 2,
-            useTabs = false,
+          complete_function_calls = true,
+          vtsls = {
+            enableMoveToFileCodeAction = true,
+            autoUseWorkspaceTsdk = true,
+            experimental = {
+              completion = {
+                enableServerSideFuzzyMatch = true,
+              },
+            },
+          },
+          typescript = {
+            updateImportsOnFileMove = { enabled = "always" },
+            suggest = {
+              completeFunctionCalls = true,
+            },
+            inlayHints = {
+              enumMemberValues = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              parameterNames = { enabled = "literals" },
+              parameterTypes = { enabled = true },
+              propertyDeclarationTypes = { enabled = true },
+              variableTypes = { enabled = false },
+            },
           },
         },
       },
